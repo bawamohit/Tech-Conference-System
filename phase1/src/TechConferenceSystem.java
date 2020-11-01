87deb64f3e4333a6b67c8968744667b45391e7c7
Anshul <anshul.agrawal@mail.utoronto.ca>
2020-11-01 19:58:37 +0530

    Started TechConferenceSystem.java

--- phase1/src/TechConferenceSystem.java
+++ phase1/src/TechConferenceSystem.java
@@ -1,5 +1,18 @@
+import javax.jws.soap.SOAPBinding;
+
 public class TechConferenceSystem {
 
+    protected UserManager um;
+    protected EventManager em;
+    protected MessageManager mm;
 
+    public TechConferenceSystem (UserManager um, EventManager em, MessageManager mm) {
+        this.um = um;
+        this.em = em;
+        this.mm = mm;
+    }
 
+    public void loginUser (String username, String password) {
+
+    }
 }
