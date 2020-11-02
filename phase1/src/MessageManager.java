49119dbac546bd1a5f45d524a97743e6f01ec13d
Max Jin <max.jin@mail.utoronto.ca>
2020-11-02 12:05:17 -0500

    Implemented sendMessage in MessageManager

--- phase1/src/MessageManager.java
+++ phase1/src/MessageManager.java
@@ -4,24 +4,28 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MessageManager {
-    private HashMap<User, HashMap<User, List<Message>>> messageList;
+    private HashMap<User, HashMap<User, ArrayList<Message>>> messageList;
     private List<User> userList; //if not needed, consider deleting
-    private List<Event> eventList;
+    private List<Event> eventList; //ArrayList maybe?
 
     public MessageManager(List<User> userList) {
         this.messageList = new HashMap<>();
         this.userList = userList;
         for (User user : userList) {
-            messageList.put(user, null);
+            HashMap<User, ArrayList<Message>> receivers = new HashMap<>();
+            messageList.put(user, receivers);
         }
     }
 
     //check if receiver valid in TechConferenceSystem
     protected void sendMessage(User sender, User receiver, LocalDateTime time, String content) {
         Message message = new Message(sender, receiver, time, content);
-        List<Message> messageValue = new ArrayList<>();
-        messageValue.add(message);
-        messageList.putIfAbsent(sender, HashMap<>());
+        if (!messageList.get(sender).containsKey(receiver)) {
+            ArrayList<Message> conversation = new ArrayList<>();
+            messageList.get(sender).put(receiver, conversation);
+        } else{
+            messageList.get(sender).get(receiver).add(message);
+        }
     }
 
     protected List<Message> getMessageHistory(User firstUser, User secondUser) {
