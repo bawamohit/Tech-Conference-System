bbff8e3e607d55463d6b58480957060af8118706
Peter <p.dang@mail.utoronto.ca>
2020-11-02 11:13:10 -0500

    dumbass

--- phase1/src/Message.java
+++ phase1/src/Message.java
@@ -4,9 +4,9 @@ public class Message {
     private User sender;
     private User receiver;
     private LocalDateTime time;
-    private String[] content;
+    private String content;
 
-    public Message(User sender, User receiver, LocalDateTime time, String[] content){
+    public Message(User sender, User receiver, LocalDateTime time, String content){
         this.sender = sender;
         this.receiver = receiver;
         this.time = time;
@@ -16,5 +16,5 @@ public class Message {
     public User getSender() { return sender; }
     public User getReceiver() { return receiver; }
     public LocalDateTime getTime() { return time; }
-    public String[] getContent() { return content; }
+    public String getContent() { return content; }
 }
--- phase1/src/MessageManager.java
+++ phase1/src/MessageManager.java
@@ -1,2 +1,31 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class MessageManager {
+    private HashMap<User, HashMap<User, List<Message>>> messageList;
+    private List<User> userList; //if not needed, consider deleting
+    private List<Event> eventList;
+
+    public MessageManager(List<User> userList) {
+        this.messageList = new HashMap<>();
+        this.userList = userList;
+        for (User user : userList) {
+            messageList.put(user, null);
+        }
+    }
+
+    //check if receiver valid in TechConferenceSystem
+    protected void sendMessage(User sender, User receiver, LocalDateTime time, String content) {
+        Message message = new Message(sender, receiver, time, content);
+        List<Message> messageValue = new ArrayList<>();
+        messageValue.add(message);
+        messageList.putIfAbsent(sender, HashMap<>());
+    }
+
+    protected List<Message> getMessageHistory(User firstUser, User secondUser) {
+        List<Message> messageHistory = new ArrayList<>();
+
+    }
 }
