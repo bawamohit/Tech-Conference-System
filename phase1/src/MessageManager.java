59ea58b9977fa075e119ff138587d71f85010b09
Max Jin <max.jin@mail.utoronto.ca>
2020-11-03 11:28:56 -0500

    Implemented deleteMessage in MessageManager

--- phase1/src/MessageManager.java
+++ phase1/src/MessageManager.java
@@ -4,32 +4,55 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MessageManager {
-    private HashMap<User, HashMap<User, ArrayList<Message>>> messageList;
+    private HashMap<User, HashMap<User, ArrayList<Message>>> conversations;
     private List<User> userList; //if not needed, consider deleting
-    private List<Event> eventList; //ArrayList maybe?
+    private List<Event> eventList; //ArrayList probably?
 
     public MessageManager(List<User> userList) {
-        this.messageList = new HashMap<>();
+        this.conversations = new HashMap<>();
         this.userList = userList;
         for (User user : userList) {
             HashMap<User, ArrayList<Message>> receivers = new HashMap<>();
-            messageList.put(user, receivers);
+            conversations.put(user, receivers);
         }
     }
 
     //check if receiver valid in TechConferenceSystem
     protected void sendMessage(User sender, User receiver, LocalDateTime time, String content) {
         Message message = new Message(sender, receiver, time, content);
-        if (!messageList.get(sender).containsKey(receiver)) {
-            ArrayList<Message> conversation = new ArrayList<>();
-            messageList.get(sender).put(receiver, conversation);
-        }
-
-        messageList.get(sender).get(receiver).add(message);
+        addReceiverConversation(sender, receiver);
+        conversations.get(sender).get(receiver).add(message);
     }
 
-    protected List<Message> getMessageHistory(User firstUser, User secondUser) {
-        List<Message> messageHistory = new ArrayList<>();
+    /**
+    * Precondition: Exists a message from sender to receiver on time
+     */
+    protected void deleteMessage(User sender, User receiver, LocalDateTime time){
+        ArrayList conversation = conversations.get(sender).get(receiver);
+        int messageIndex = binarySearchMessage(conversation, 0 , conversation.size(), time);
+        conversation.remove(messageIndex); //store edit history?
+    }
 
+    protected ArrayList<Message> getConversation(User firstUser, User secondUser) {
+        return conversations.get(firstUser).get(secondUser);
+    }
+
+    private void addReceiverConversation(User sender, User receiver){
+        if (!conversations.get(sender).containsKey(receiver)) {
+            ArrayList<Message> conversation = new ArrayList<>();
+            conversations.get(sender).put(receiver, conversation);
+        }
+    }
+
+    private int binarySearchMessage(ArrayList conversation, int startIndex, int endIndex, LocalDateTime time){
+        int midIndex = (startIndex + endIndex) / 2;
+        LocalDateTime midMessageTime = ((Message)conversation.get(midIndex)).getTime();
+        if(midMessageTime.isEqual(time)){
+            return midIndex;
+        } else if(midMessageTime.isBefore(time)){
+            return binarySearchMessage(conversation, startIndex, midIndex, time);
+        } else{
+            return binarySearchMessage(conversation, midIndex + 1, endIndex, time);
+        }
     }
 }
