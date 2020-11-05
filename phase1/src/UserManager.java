4e38915df4ca7168ce343cb361028de930cbcccd
bawamohit <bawamohit1@outlook.com>
2020-11-04 19:20:53 -0500

    (11-04) Mohit added getUsers, setUsers, verifyLogin, updatePassword, and getUserType to UserManager

--- phase1/src/UserManager.java
+++ phase1/src/UserManager.java
@@ -8,4 +8,29 @@ public class UserManager {
     public UserManager () {
         this.users = new ArrayList<>();
     }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public User verifyLogin(String username, String password) { //ask about returning null
+        for (User u : users) {
+            if (u.getUsername().equals(username) && u.getPassword().equals(password)) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    public void updatePassword(User user, String newPassword) {
+        user.setPassword(newPassword);
+    }
+
+    public UserType getUserType(User user){
+        return user.getUserType();
+    }
 }
