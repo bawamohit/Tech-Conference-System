79ceb50448630468e174fe4bbbaf556bb8991d4f
Peter <p.dang@mail.utoronto.ca>
2020-11-05 08:55:44 -0500

    alpha version

--- phase1/src/Message.java
+++ phase1/src/Message.java
@@ -1,20 +1,20 @@
 import java.time.LocalDateTime;
 
 public class Message {
-    private User sender;
-    private User receiver;
+    private String sender;
+    private String receiver;
     private LocalDateTime time;
     private String content;
 
-    public Message(User sender, User receiver, LocalDateTime time, String content){
+    public Message(String sender, String receiver, LocalDateTime time, String content){
         this.sender = sender;
         this.receiver = receiver;
         this.time = time;
         this.content = content;
     }
 
-    public User getSender() { return sender; }
-    public User getReceiver() { return receiver; }
+    public String getSender() { return sender; }
+    public String getReceiver() { return receiver; }
     public LocalDateTime getTime() { return time; }
     public String getContent() { return content; }
 }
--- phase1/src/MessageManager.java
+++ phase1/src/MessageManager.java
@@ -4,54 +4,68 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MessageManager {
-    private HashMap<User, HashMap<User, ArrayList<Message>>> conversations;
-    private List<User> userList; //if not needed, consider deleting
-    private List<Event> eventList; //ArrayList probably?
+    private HashMap<String, HashMap<String, ArrayList<Message>>> conversations;
+    private ArrayList<String> usernameList; //if not needed, consider deleting
 
-    public MessageManager(List<User> userList) {
+    // controller does everything ??????
+    public MessageManager(List<String> userList) {
         this.conversations = new HashMap<>();
-        this.userList = userList;
-        for (User user : userList) {
-            HashMap<User, ArrayList<Message>> receivers = new HashMap<>();
+        usernameList = new ArrayList<>();
+        for (String user : userList) {
+            usernameList.add(user);
+            HashMap<String, ArrayList<Message>> receivers = new HashMap<>();
             conversations.put(user, receivers);
         }
     }
 
-    //check if receiver valid in TechConferenceSystem
-    protected void sendMessage(User sender, User receiver, LocalDateTime time, String content) {
-        Message message = new Message(sender, receiver, time, content);
-        addReceiverConversation(sender, receiver);
-        conversations.get(sender).get(receiver).add(message);
+    // GROUP CHAT MOTHERCUKERS ?????????????
+    protected void sendMessage(String sender, String receiver, LocalDateTime time, String content) {
+        addMessage(sender, receiver, time, content);
+        addMessage(receiver, sender, time, content);
     }
 
     /**
     * Precondition: Exists a message from sender to receiver on time
      */
-    protected void deleteMessage(User sender, User receiver, LocalDateTime time){
-        ArrayList conversation = conversations.get(sender).get(receiver);
+    protected void deleteMessage(String sender, String receiver, LocalDateTime time){
+        ArrayList<Message> conversation = conversations.get(sender).get(receiver);
         int messageIndex = binarySearchMessage(conversation, 0 , conversation.size(), time);
         conversation.remove(messageIndex); //store edit history?
     }
 
-    protected ArrayList<Message> getConversation(User firstUser, User secondUser) {
+    protected ArrayList<Message> getConversation(String firstUser, String secondUser) {
         return conversations.get(firstUser).get(secondUser);
     }
 
-    private void addReceiverConversation(User sender, User receiver){
+
+    protected void messageEvent(String sender, List<String> userList, LocalDateTime time, String content) {
+        for (String receiver : userList) {
+            sendMessage(sender, receiver, time, content);
+        }
+    }
+
+    //check if receiver valid in TechConferenceSystem
+    protected void addMessage(String firstUser, String secondUser, LocalDateTime time, String content) {
+        Message message = new Message(firstUser, secondUser, time, content);
+        addReceiverConversation(firstUser, secondUser);
+        conversations.get(firstUser).get(secondUser).add(message);
+    }
+
+    private void addReceiverConversation(String sender, String receiver){
         if (!conversations.get(sender).containsKey(receiver)) {
             ArrayList<Message> conversation = new ArrayList<>();
             conversations.get(sender).put(receiver, conversation);
         }
     }
 
-    private int binarySearchMessage(ArrayList conversation, int startIndex, int endIndex, LocalDateTime time){
+    private int binarySearchMessage(ArrayList<Message> conversation, int startIndex, int endIndex, LocalDateTime time){
         int midIndex = (startIndex + endIndex) / 2;
-        LocalDateTime midMessageTime = ((Message)conversation.get(midIndex)).getTime();
+        LocalDateTime midMessageTime = (conversation.get(midIndex)).getTime();
         if(midMessageTime.isEqual(time)){
             return midIndex;
         } else if(midMessageTime.isBefore(time)){
             return binarySearchMessage(conversation, startIndex, midIndex, time);
-        } else{
+        } else {
             return binarySearchMessage(conversation, midIndex + 1, endIndex, time);
         }
     }
