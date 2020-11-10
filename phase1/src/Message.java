ce1ac79e0d71b2305b69582a3b2e4b9fd2898574
Peter <p.dang@mail.utoronto.ca>
2020-11-10 11:51:43 -0500

    Added ID for Message

--- phase1/src/Message.java
+++ phase1/src/Message.java
@@ -1,20 +1,24 @@
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class Message {
     private String sender;
     private String receiver;
     private LocalDateTime time;
     private String content;
+    private UUID id;
 
     public Message(String sender, String receiver, String content){
         this.sender = sender;
         this.receiver = receiver;
         this.time = LocalDateTime.now();
         this.content = content;
+        this.id = UUID.randomUUID();
     }
 
     public String getSender() { return sender; }
     public String getReceiver() { return receiver; }
     public LocalDateTime getTime() { return time; }
     public String getContent() { return content; }
+    public UUID getID() { return id; }
 }
