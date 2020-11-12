68cd451c5ad4e0778f2aed8e3e6b00e7b00029ae
bawamohit <bawamohit1@outlook.com>
2020-11-12 14:26:57 -0500

    Made more changes to TechConferenceSystem

--- phase1/src/TechConferenceSystem.java
+++ phase1/src/TechConferenceSystem.java
@@ -80,7 +80,7 @@ public class TechConferenceSystem {
                     accountType = UserType.SPEAKER;
                 }
 
-                if (accountType.equals(null)){
+                if (accountOption.equals(null)){
                     System.out.println("ERROR that is not a valid choice.\n\n");
                 }
                 else {
