ef5936965f01e01a79e1e034e603fec313886c70
Peter <p.dang@mail.utoronto.ca>
2020-11-12 16:33:00 -0500

    Added menu and messaging options in TCS

--- phase1/src/Presenter.java
+++ phase1/src/Presenter.java
@@ -1,16 +1,16 @@
 public class Presenter {
+
     public void printWelcome(){
-        System.out.println("Welcome to the lamest Technology conference ever!!!\n" +
-                "Press 1 to login to your account\n" +
-                "Press 2 to sign up for an account");
+        System.out.println("\nWelcome to the lamest Technology conference ever!!!\n" +
+                "\nPress 1 to login to your account\n" +
+                "\nPress 2 to sign up for an account\n");
     }
 
-    public void printAskID(){
-        System.out.println("\nPlease enter your user ID");
-    }
-    public void printAskPassword(){
-        System.out.println("Please enter your password");
-    }
+    public void printAsk(String ask) { System.out.println("\nPlease enter your " + ask); }
+
+    public void printInvalidInput() { System.out.println("\nInvalid input.\n");}
+
+    public void printUsernameExists() { System.out.println("\nUsername already exists\n");}
 
     public void printWrongAccountInfo(){
         System.out.println("\nOOPS looks like you entered the wrong login information," +
@@ -19,21 +19,25 @@ public class Presenter {
                 "Or enter 'sign up' to sign up for an account.");
     }
 
-    public void printPrompt(){
-        System.out.println("Pick a number to perform the corresponding action, " +
-                "or don't, I'm not yo dad\n\n ");
+    public void printUnderConstruction(){
+        System.out.println("Sorry! This feature is currently under construction.");
     }
 
-    public void printUserType() {
-        printPrompt();
+    public void printPrompt(){
+        System.out.println("\nPick a number to perform the corresponding action, " +
+                "or don't, I'm not yo dad\n ");
+    }
+
+    public void printAskUserType() {
+        System.out.println("\nPlease enter your account type.\n");
         System.out.println("0. Attendee\n" +
                 "1. Organizer\n" +
-                "2. Speaker");
+                "2. Speaker\n");
     }
 
     public void printAttendeeMenu(){
         printPrompt();
-        System.out.println("0. Logout\n" +
+        System.out.println("\n0. Logout\n" +
                 "1. Messaging\n" +
                 "2. View All Events\n" +
                 "3. View My Events\n" +
@@ -43,21 +47,21 @@ public class Presenter {
 
     public void printOrganizerMenu(){
         printAttendeeMenu();
-        System.out.println("7. Create Event\n" +
-                "8. Reschedule Event\n" +
-                "9. Remove Event\n" +
-                "10. Create Speaker Account\n" +
-                "11. Create Room");
+        System.out.println("6. Create Event\n" +
+                "7. Reschedule Event\n" +
+                "8. Remove Event\n" +
+                "9. Create Speaker Account\n" +
+                "10. Create Room\n");
     }
 
     public void printSpeakerMenu(){
         printAttendeeMenu();
-        System.out.println("7. View My Talks");
+        System.out.println("6. View My Talks\n");
     }
 
     public void printAttendeeMessageMenu() {
         printPrompt();
-        System.out.println("0. Message\n" +
+        System.out.println("\n0. Message\n" +
                 "1. Edit a message\n" +
                 "2. Delete a message");
     }
@@ -65,22 +69,19 @@ public class Presenter {
     public void printOrganizerMessageMenu() {
         printAttendeeMessageMenu();
         System.out.println("3. Message all speakers\n" +
-                "4. Message all attendees");
+                "4. Message all attendees\n");
     }
 
     public void printSpeakerMessageMenu() {
         printAttendeeMessageMenu();
-        System.out.println("3. Message all Attendees of your talk(s).");
+        System.out.println("3. Message all Attendees of your talk(s).\n");
     }
 
     public void printBack(){
         System.out.println("Enter 'b' to go back to previous menu");
     }
+
     public void printIOException(){
         System.out.println("IOException");
-    }
-
-    public void printUnderConstruction(){
-        System.out.println("Sorry! This feature is currently under construction.");
     }
 }
