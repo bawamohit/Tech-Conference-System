3df634d2459985176d2f41acf4123dbbd34739ff
Max Jin <max.jin@mail.utoronto.ca>
2020-11-12 20:39:52 -0500

    Made specific Gateway Class, working on general Gateway class dont @me

---
+++ phase1/src/MessageGateway.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class MessageGateway {
+    public MessageManager readFromFile(String path) throws ClassNotFoundException {
+
+        try {
+            InputStream file = new FileInputStream(path);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+
+            // deserialize the MessageManager
+            MessageManager messageManager= (MessageManager) input.readObject();
+            input.close();
+            return messageManager;
+        } catch (IOException ex) {
+            System.out.println("Cannot read from input file, returning a new MessageManager.");
+            return new MessageManager();
+        }
+    }
+
+    public void saveToFile(String filePath, MessageManager messageManager) throws IOException {
+
+        OutputStream file = new FileOutputStream(filePath);
+        OutputStream buffer = new BufferedOutputStream(file);
+        ObjectOutputStream output = new ObjectOutputStream(buffer);
+
+        // serialize the messageManager object
+        output.reset();
+        output.writeObject(messageManager);
+        output.close();
+    }
+}
--- phase1/src/UserGateway.java
+++ phase1/src/UserGateway.java
@@ -1,4 +1,33 @@
-public class UserGateway {
+import java.io.*;
 
+public class UserGateway {
+    public UserManager readFromFile(String path) throws ClassNotFoundException {
+
+        try {
+            InputStream file = new FileInputStream(path);
+            InputStream buffer = new BufferedInputStream(file);
+            ObjectInput input = new ObjectInputStream(buffer);
+
+            // deserialize the UserManager
+            UserManager userManager = (UserManager) input.readObject();
+            input.close();
+            return userManager;
+        } catch (IOException ex) {
+            System.out.println("Cannot read from input file, returning a new UserManager.");
+            return new UserManager();
+        }
+    }
+
+    public void saveToFile(String filePath, UserManager userManager) throws IOException {
+
+        OutputStream file = new FileOutputStream(filePath);
+        OutputStream buffer = new BufferedOutputStream(file);
+        ObjectOutputStream output = new ObjectOutputStream(buffer);
+
+        // serialize the UserManager object
+        output.reset();
+        output.writeObject(userManager);
+        output.close();
+    }
 
 }
