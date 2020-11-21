aca47e47083457beaa1eff3e9eaad715fa48363f
Max Jin <max.jin@mail.utoronto.ca>
2020-11-20 19:30:25 -0500

    Allowed back option for deleteMessage and viewInbox

--- phase1/src/Controllers/UserSystem.java
+++ phase1/src/Controllers/UserSystem.java
@@ -7,7 +7,29 @@ public abstract class UserSystem {
 
     abstract void run(String username, TechConferenceSystem tcs);
 
-    // Implements the general message system for all user types
+    /**
+     * Implements a method used to format a list of strings to an numbered string.
+     * This is used to format a list of information to a string, that shows each element inside the list as a numbered
+     * string format.
+     */
+    protected String formatInfo(List<String> strings){
+        StringBuilder info = new StringBuilder();
+        for (int i = 0; i < strings.size(); i++){
+            info.append("\n").append(i).append(". ").append(strings.get(i));
+        }
+
+        return info.toString();
+    }
+
+    protected String validInput(String pattern, Scanner scanner, TechConferenceSystem tcs){
+        String input = scanner.nextLine();
+        while(!input.matches(pattern)){
+            tcs.getPresenter().printInvalidInput();
+            input = scanner.nextLine();
+        }
+        return input;
+    }
+
     protected void helperMessageSystem(String username, String choice, Scanner scanner, TechConferenceSystem tcs){
         switch (choice) {
             case "1":
@@ -30,7 +52,6 @@ public abstract class UserSystem {
         }
     }
 
-    // Helper method, sends a message from the User to a receiver (user input) with content (user input)
     private void sendMessage(String username, Scanner scanner, TechConferenceSystem tcs){
         String receiver;
         while(true) {
@@ -52,31 +73,34 @@ public abstract class UserSystem {
         tcs.getPresenter().printMessageSent();
     }
 
-    // Helper method, deletes a message in the User's inboxes (user chooses the message)
-    private void deleteMessage(String username, Scanner scanner, TechConferenceSystem tcs){
-        String inboxChoice = viewInbox(username, scanner, tcs);
-        int inboxSize = tcs.getMM().getInbox(username, inboxChoice).size();
-        int index;
-        while(true) {
-            tcs.getPresenter().printAskWhichMessage();
-            index = scanner.nextInt();
-            if (0 <= index && index < inboxSize) {
-                break;
-            } else {
-                tcs.getPresenter().printInvalidInput();
-            }
-        }
-        tcs.getMM().deleteMessage(tcs.getMM().getInbox(username, inboxChoice).get(index));
+    // TODO: Use this method or delete it
+    private void editMessage(String username, Scanner scanner, TechConferenceSystem tcs){
+        tcs.getPresenter().printUnderConstruction();
     }
 
-    // Helper method, views an inbox between the User and another chosen (by input) user
+    private void deleteMessage(String username, Scanner scanner, TechConferenceSystem tcs){
+        String inboxChoice = viewInbox(username, scanner, tcs);
+        if(inboxChoice.equals("")) return;
+        int inboxSize = tcs.getMM().getInbox(username, inboxChoice).size();
+        String index;
+        tcs.getPresenter().printAskWhichMessage();
+        tcs.getPresenter().printBackToMainMenu();
+        index = validInput("^[0-" + (inboxSize - 1) + "]$|^.{0}$", scanner, tcs);
+        if(index.equals("")) return;
+        tcs.getMM().deleteMessage(tcs.getMM().getInbox(username, inboxChoice).get(Integer.parseInt(index)));
+        tcs.getPresenter().printMesaageDeleted();
+    }
+
     private String viewInbox(String username, Scanner scanner, TechConferenceSystem tcs){
         String inboxChoice;
         while (true) {
             tcs.getPresenter().printAskWhichInbox();
             tcs.getPresenter().printUCReturns(tcs.getMM().getInboxes(username));
+            tcs.getPresenter().printBackToMainMenu();
             inboxChoice = scanner.nextLine();
-            if (tcs.getMM().getInboxes(username).contains(inboxChoice)) {
+            if (inboxChoice.equals("")){
+                return inboxChoice;
+            } else if (tcs.getMM().getInboxes(username).contains(inboxChoice)) {
                 break;
             } else {
                 tcs.getPresenter().printInvalidInput();
@@ -85,36 +109,8 @@ public abstract class UserSystem {
         List<String> inbox = tcs.getMM().getInboxString(username, inboxChoice);
         String inboxToString = formatInfo(inbox);
         tcs.getPresenter().printUCReturns(inboxToString);
+
         return inboxChoice;
-    }
-
-    /**
-     * Implements a method used to format a list of strings to an numbered string.
-     * This is used to format a list of information to a string, that shows each element inside the list as a numbered
-     * string format.
-     */
-    protected String formatInfo(List<String> strings){
-        StringBuilder info = new StringBuilder();
-        for (int i = 0; i < strings.size(); i++){
-            info.append("\n").append(i).append(". ").append(strings.get(i));
-        }
-
-        return info.toString();
-    }
-
-    // Helper method, checks whether the user's input is valid in respect to the menu options
-    protected String validInput(String pattern, Scanner scanner, TechConferenceSystem tcs){
-        String input = scanner.nextLine();
-        while(!input.matches(pattern)){
-            tcs.getPresenter().printInvalidInput();
-            input = scanner.nextLine();
-        }
-        return input;
-    }
-
-    // TODO: Use this method or delete it
-    private void editMessage(String username, Scanner scanner, TechConferenceSystem tcs){
-        tcs.getPresenter().printUnderConstruction();
     }
 }
 
