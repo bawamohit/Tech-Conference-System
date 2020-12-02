2c4791c86c574bdb46c1afb8f8c22a21035e0a2e
hyunnaye <nayeonhyun802@gmail.com>
2020-12-01 23:37:29 -0500

    event fxml

---
+++ phase2/src/GUI/EventMenu/EventMenuController.java
@@ -0,0 +1,40 @@
+package GUI.EventMenu;
+
+import GUI.GUIController;
+import GUI.MainController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+
+public class EventMenuController implements GUIController {
+    private MainController mainController;
+    @FXML private Text prompt;
+
+    @Override
+    public void initData(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+    @FXML
+    protected void handleAvailEventButtonAction(ActionEvent event) {
+        prompt.setText("sike");
+    }
+
+    @FXML
+    protected void handleMyEventsButtonAction(ActionEvent actionEvent) {
+        prompt.setText("sike");
+    }
+
+    @FXML
+    public void handleSignUpEventButtonAction(ActionEvent event) {
+        prompt.setText("SIKE");
+    }
+
+    @FXML
+    public void handleCancelEventButtonAction(ActionEvent event) throws IOException {
+        prompt.setText("sike");
+    }
+}
+
