dd9f1ffe31b51dd633049c4510cef8590c24a76a
hyunnaye <nayeonhyun802@gmail.com>
2020-12-03 14:30:35 -0500

    resized welcome for the water splash

--- phase2/src/GUI/Prototype.java
+++ phase2/src/GUI/Prototype.java
@@ -21,9 +21,9 @@ public class Prototype extends Application{
     private EventGateway eventGateway;
     private MessageGateway messageGateway;
     private String welcomeFXMLPath = "Welcome.fxml";
-    private File userInfo = new File("./phase2/src/Data/userManager.json");
-    private File eventInfo = new File("./phase2/src/Data/eventManager.json");
-    private File messageInfo = new File("./phase2/src/Data/messageManager.json");
+    private File userInfo = new File("./src/Data/userManager.json");
+    private File eventInfo = new File("./src/Data/eventManager.json");
+    private File messageInfo = new File("./src/Data/messageManager.json");
     private UserManager userManager;
     private EventManager eventManager;
     private MessageManager messageManager;
@@ -44,7 +44,7 @@ public class Prototype extends Application{
         FXMLLoader loader = new FXMLLoader(getClass().getResource(welcomeFXMLPath));
 
         Parent root = loader.load();
-        Scene scene = new Scene(root, 300, 300);
+        Scene scene = new Scene(root, 750, 650);
 
         primaryStage.setTitle("Conference");
         primaryStage.setScene(scene);
