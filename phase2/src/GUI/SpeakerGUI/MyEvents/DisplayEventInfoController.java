b5f6a516c16d053d57c1c622e8f87ced4bd06790
Peter <p.dang@mail.utoronto.ca>
2020-12-12 21:28:41 -0500

    Speaker Message Event Cleaned up Speaker

--- phase2/src/GUI/SpeakerGUI/MyEvents/DisplayEventInfoController.java
+++ phase2/src/GUI/SpeakerGUI/MyEvents/DisplayEventInfoController.java
@@ -5,11 +5,30 @@ import GUI.GUIController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import java.util.List;
 
 public class DisplayEventInfoController extends GUI.EventInfoController implements GUIController {
+
+    @FXML Button messageButton;
+    @FXML TextField textField;
 
     @Override
     public void initialize() {
         super.initialize();
+        textField.setVisible(false);
+    }
+
+    @FXML
+    protected void handleMessageButton(ActionEvent actionEvent) {
+        if (!textField.isVisible()) {
+            textField.setVisible(true);
+        } else {
+            String content = textField.getText();
+            if (!content.equals("")) {
+                getMessageManager().messageAll(getUsername(), getEventManager().getEventAttendees(getEventID()), content);
+            }
+        }
     }
 }
