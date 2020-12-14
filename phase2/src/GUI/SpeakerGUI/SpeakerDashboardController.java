bb6dbf6f091e80bcdb51cdb0517a0b5145ea2971
Peter <p.dang@mail.utoronto.ca>
2020-12-13 20:32:05 -0500

    Speaker javadocs

--- phase2/src/GUI/SpeakerGUI/SpeakerDashboardController.java
+++ phase2/src/GUI/SpeakerGUI/SpeakerDashboardController.java
@@ -9,6 +9,9 @@ import javafx.scene.layout.AnchorPane;
 
 import java.util.Observable;
 
+/**
+ * Dashboard for Speakers
+ */
 public class SpeakerDashboardController extends UserDashboardController {
     @FXML private AnchorPane anchorPane;
     @FXML private SplitPane splitPane;
@@ -17,20 +20,32 @@ public class SpeakerDashboardController extends UserDashboardController {
     @FXML private Button inboxButton;
     @FXML private Button messageTalkButton;
 
+    /**
+     * Initializes the dashboard for speakers. Displays the speaker's talks
+     */
     public void initialize(){
         super.initData("/GUI/SpeakerGUI/MyEvents/DisplayTalks");
     }
 
+    /**
+     * Handles the action when the Inbox button is clicked. Displays the speaker's inbox
+     */
     @FXML
     protected void handleInboxButtonAction(ActionEvent event) {
         loadSubScene("/GUI/Message/Message");
     }
 
+    /**
+     * Handles the action when the My Events button is clicked. Displays the speaker's talks
+     */
     @FXML protected void handleMyEventButtonAction(ActionEvent event) {
         loadSubScene("/GUI/SpeakerGUI/MyEvents/DisplayTalks");
         observeDisplayEvents();
     }
 
+    /**
+     * Update the scene after an action is done
+     */
     @Override
     public void update(Observable o, Object arg) {
         loadSubScene("/GUI/SpeakerGUI/MyEvents/MyEvents");
